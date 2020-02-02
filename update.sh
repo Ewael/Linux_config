68c04e9d85be2c6e2fef636b11efbdf04feccea3
Turodoras <turodoras@gmail.com>
2020-02-02 18:34:45 +0100

    [UPDATE][ADD] better args

--- update.sh
+++ update.sh
@@ -4,45 +4,79 @@
 # It takes one argument which is what want to update
 # Usage: ./update.sh arg
 
-# all
-all=false
-if [ "$1" = "all" ]; then
-    all=true
+# help
+if [ "$1" = "--help" ]; then
+    echo "Exec ./update.sh with one of the following arg:
+
+        chromium    - chromium flags
+        general     - xinitrc and Xresources files
+        i3          - i3 config file
+        redshift    - redshift config file
+        vim         - vim config file
+        zsh         - zsh config file
+
+        arch        - general, i3, redshift, vim, zsh
+        kali        - chromium, redshift, vim, zsh"
 fi
 
-# zsh
-if [ "$1" = "zsh" ] || [ "$all" = true ]; then
-    echo "Updating zsh config file"
-    cp zsh/.zshrc ~
+arch=false
+kali=false
+
+####### GENERAL ARGS #######
+
+# arch
+if [ "$1" = "arch" ]; then
+    echo "# Updating general, i3, redshift, vim, zsh files"
+    arch=true
 fi
 
-# system
-if [ "$1" = "general" ] || [ "$all" = true ]; then
-    echo "Updating .X**** files"
+# kali
+if [ "$1" = "kali" ]; then
+    echo "# Updating chromium, redshift, vim, zsh files"
+    kali=true
+fi
+
+####### INDIV ######
+
+# chromium
+if [ "$1" = "chromium" ] || [ "$kali" = true ]; then
+    echo "-- Updating chromium flags"
+    cp chromium/default-flags /etc/chromium.d/default-flags
+fi
+
+# general
+if [ "$1" = "general" ] || [ "$arch" = true ]; then
+    echo "-- Updating general files"
     cp general/.Xresources general/.xinitrc ~
 fi
 
-# vim
-if [ "$1" = "vim" ] || [ "$all" = true ]; then
-    echo "Updating vim config file"
-    cp vim/.vimrc ~
-fi
-
 # i3
-if [ "$1" = "i3" ] || [ "$all" = true ]; then
-    echo "Updating i3 config file"
+if [ "$1" = "i3" ] || [ "$arch" = true ]; then
+    echo "-- Updating i3 config file"
     cp i3/config ~/.i3
 fi
-# redshift
 
-if [ "$1" = "red" ] || [ "$all" = true ]; then
-    echo "Updating redshift config file"
+# redshift
+if [ "$1" = "red" ] || [ "$arch" = true ] || [ "$kali" = true ]; then
+    echo "-- Updating redshift config file"
     cp redshift/redshift.conf ~/.config
 fi
 
-# wrong arg
-if [ $# != 1 ]; then
-    echo "Please choose a valid option"
+# vim
+if [ "$1" = "vim" ] || [ "$arch" = true ] || [ "$kali" = true ]; then
+    echo "-- Updating vim config file"
+    cp vim/.vimrc ~
 fi
 
-echo "Done"
+# zsh
+if [ "$1" = "zsh" ] || [ "$arch" = true ] || [ "$kali" = true ]; then
+    echo "-- Updating zsh config file"
+    cp zsh/.zshrc ~
+fi
+
+####### END #######
+
+# wrong arg
+if [ $# != 1 ]; then
+    echo "Wrong number of arguments, please exec ./update.sh --help"
+fi

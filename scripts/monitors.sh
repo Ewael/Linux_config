b135a5150c7895e90f58ee505c2059044cae3c0a
Turodoras <turodoras@gmail.com>
2021-09-27 00:34:03 +0200

    [I3] adapt to new config

--- scripts/monitors.sh
+++ scripts/monitors.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 
 xrandr \
-    --output DVI-I-1 --mode 1920x1080 --pos 0x0 --rotate normal \
-    --output DVI-D-1 --mode 1920x1080 --pos 3851x0 --rotate normal \
-    --output HDMI-1 --primary --mode 1920x1080 --pos 1920x0 --rotate normal \
-    --output DP-1 --off
+    --output DVI-D-0 --primary --mode 1920x1080 --pos 0x460 --rotate normal \
+    --output HDMI-0 --mode 1920x1080 --pos 1920x0 --rotate right \
+    --output DP-1 --off \
+    --output DP-1 --off \
+    --output DVI-I-0 --off \
+    --output DVI-I-1 --off
+
+
+nitrogen --restore
+nitrogen --set-auto ~/Linux_config/wallpapers/orange_city.jpg --head=0
+nitrogen --set-auto ~/Linux_config/wallpapers/orange_vertical.jpg --head=1

c6d79a0d527aca4066295a5a350fa56ae9add9b4
Ewael <turodoras@gmail.com>
2022-02-04 12:00:20 +0100

    Fix monitors script

--- scripts/monitors.sh
+++ scripts/monitors.sh
@@ -5,6 +5,6 @@
 
 xrandr \
     --output DVI-D-1 --primary --mode 1920x1080 --pos 0x455 --rotate normal \
-    --output HDMI-1 --mode 1920x1080 --pos 1920x0 --rotate left \
+    --output HDMI-1 --mode 0x47 --pos 1920x0 --rotate left \
     --output DVI-I-1 --off \
     --output DP-1 --off
--- update.sh
+++ update.sh
@@ -25,3 +25,8 @@ if check "Update .vimrc";
 then
     cp -v dotfiles/vim/.vimrc ~
 fi
+
+if check "Update monitors config";
+then
+    sudo cp -v scripts/monitors.sh /etc/profile.d
+fi

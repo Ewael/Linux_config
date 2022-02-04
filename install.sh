f8836a9a32fe3a552370319a1acb253ce51a159b
Ewael <turodoras@gmail.com>
2022-02-04 11:21:08 +0100

    Fix chrome package to install

--- install.sh
+++ install.sh
@@ -54,7 +54,7 @@ then
     git clone https://aur.archlinux.org/google-chrome.git
     cd google-chrome
     makepkg -s
-    sudo pacman -U --noconfirm google-chrome*
+    sudo pacman -U --noconfirm google-chrome*.zst
 fi
 
 # install packages for YCM

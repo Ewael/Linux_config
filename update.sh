f05d8eadfdb2c0bd654f169584fc321cd64d3734
Ewael <turodoras@gmail.com>
2022-02-05 11:58:19 +0100

    Add / to dir paths

--- update.sh
+++ update.sh
@@ -18,27 +18,27 @@ check ()
 
 if check "Update .zshrc";
 then
-    cp -v dotfiles/zsh/.zshrc ~
+    cp -v dotfiles/zsh/.zshrc ~/
 fi
 
 if check "Update .vimrc";
 then
-    cp -v dotfiles/vim/.vimrc ~
+    cp -v dotfiles/vim/.vimrc ~/
 fi
 
 if check "Update terminator";
 then
-    cp -v dotfiles/terminator/config ~/.config/terminator
+    cp -v dotfiles/terminator/config ~/.config/terminator/
 fi
 
 if check "Update monitors config";
 then
     if check "Is this home config";
     then
-        cp -v scripts/monitors.sh.desktop ~/.config/autostart
+        cp -v scripts/monitors.sh.desktop ~/.config/autostart/
     fi
     if check "Is this LSE config";
     then
-        cp -v scripts/lse_monitors.sh.desktop ~/.config/autostart
+        cp -v scripts/lse_monitors.sh.desktop ~/.config/autostart/
     fi
 fi
